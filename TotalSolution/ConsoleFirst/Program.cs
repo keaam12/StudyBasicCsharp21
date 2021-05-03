39ffecd1b05366951b6bca5753485b09d2547d5e
홍건의 <keaam12@google.com>
2021-05-03 10:41:24 +0900

    첫번째 commit

--- TotalSolution/ConsoleFirst/Program.cs
+++ TotalSolution/ConsoleFirst/Program.cs
@@ -7,6 +7,7 @@ namespace ConsoleFirst
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!"); //주석
+            Console.WriteLine("Second change");
         }
     }
 }
